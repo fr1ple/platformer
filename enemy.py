d6e17f3a79896a2499710a23da5733b9757d9293
Misha Agaev <mishuxa0611@gmail.com>
2019-03-01 10:37:59 +0300

    lesson 20

--- enemy.py
+++ enemy.py
@@ -1,6 +1,8 @@
 import pygame
 from settings import enemy_image
 from constants import *
+from random import randint
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -15,4 +17,6 @@ class Enemy(pygame.sprite.Sprite):
           )
         
         self.rect = self.image.get_rect()
-        self.rect.midtop = (WIDTH / 2, 20 )
+        YAN_EAT_DOGE = self.rect.width/2 
+        YANA = randint(0 + YAN_EAT_DOGE, WIDTH - YAN_EAT_DOGE)
+        self.rect.midtop = (YANA ,20 )
