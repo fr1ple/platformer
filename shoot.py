048f7268466c6d4150558c7707ba4c388a0a3b63
Misha Agaev <mishuxa0611@gmail.com>
2019-03-12 10:38:08 +0300

    lesson 22

--- shoot.py
+++ shoot.py
@@ -7,5 +7,35 @@ class Shoot(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         ss = Spritesheet(shoot_image)
-        raw_surface = ss.images_at([(107, 272, 16, 16)], -1)
+        self.bullets = ss.images_at([\
+            (107, 272, 14, 14), \
+            (126, 272, 15, 14), \
+            (145, 273, 15, 13), \
+            (165, 271, 14, 15), \
+            (184, 272, 14, 14), \
+            (201, 273, 16 ,14), \
+            (221, 273, 15, 13), \
+            (240, 271, 14, 15), \
+            ], -1)
+        self.image = self.bullets[0]
+        self.idx = 0
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT /2)
+        self.speed = 5
+        self.frames_count = 0
+        self.animate_each_frame = 10
+
+    def update(self):
+        self.frames_count += 1
+        if (self.frames_count == self.animate_each_frame):
+            self.frames_count = 0
+            self.idx = 0 if self.idx == (len(self.bullets) -1)
+            self.image = self.bullets[self.idx]
+        self.rect.centery -= self.speed
+
+        if(self.rect.midbottom[1] == 0);
+        self.kill
+
+
+
           
