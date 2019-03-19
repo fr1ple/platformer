6c4bb009f746673f27932bcc3c221e47318ae0e1
Misha Agaev <mishuxa0611@gmail.com>
2019-03-19 10:33:54 +0300

    lesson 23

--- level1.py
+++ level1.py
@@ -1,2 +1,12 @@
 import pygame
-from settings import 
+from settings import level1_image
+from constants import *
+from spritesheet import *
+
+
+class Level1(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        raw_image = pygame.image.load(level1_image).convert()
+        w = raw_image.get_rect().width
+        scale_coefficent= WIDTH / w
--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ player_image = os.path.join(game_folder, 'resources', 'img', 'player.png')
 enemy_image = os.path.join(game_folder, 'resources','img', 'RD2.png' )
 
 shoot_image = os.path.join(game_folder, 'resources','img','shoot.png' )
+
+level1_image = os.path.join(game_folder, 'resources','img','level1.png')
